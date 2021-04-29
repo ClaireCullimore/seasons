dc8254cc99cec699385ed07a145c4ca293d8f21a
CLaire Cullimore <cascullimore@gmail.com>
2021-04-29 15:55:20 +0100

    Lifecycle Methods

--- src/index.js
+++ src/index.js
@@ -8,18 +8,16 @@ class App extends React.Component {
 
     // This is the only time we do direct assignment to this.state
     this.state = { lat: null, errorMessage: '' };
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({
-          errorMessage: err.message
-        });
-      }
-    );
  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+
    // React says we have to define render!!
 
 //   render() {
