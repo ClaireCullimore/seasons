47354e218ad437758f16077aacc9d2b5fb88af9f
CLaire Cullimore <cascullimore@gmail.com>
2021-04-29 12:35:42 +0100

    setState

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // This is the only time we do direct assignment to this.state
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
+ }
+   // React says we have to define render!!
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDom.render(<App />, document.querySelector('#root'))
