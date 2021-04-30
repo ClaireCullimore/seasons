4d5187dba0d6f764ccdac5915e47144fa94012ac
CLaire Cullimore <cascullimore@gmail.com>
2021-04-30 10:15:43 +0100

    Determing Season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+// Determine what the season is
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0  ? 'Summer' : 'Winter';
+  } else {
+    return lat > 0 ? 'Winter' : 'Summer';
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.late, new Date().getMonth());
+
+  console.log(season);
+
   return <div>Season Display</div>
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+ //  constructor(props) {
+ //    super(props);
 
-    // This is the only time we do direct assignment to this.state
-    this.state = { lat: null, errorMessage: '' };
- }
+ //    // This is the only time we do direct assignment to this.state
+ //    this.state = { lat: null, errorMessage: '' };
+ // }
 
+    state = { lat: null, errorMessage: '' };
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
@@ -37,7 +38,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>;
