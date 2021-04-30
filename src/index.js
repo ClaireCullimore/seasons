fa57ac2b58dc13b12dcc5f4a28b37bef5dc70566
CLaire Cullimore <cascullimore@gmail.com>
2021-04-30 11:41:02 +0100

    changing conditionals in render method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import SemanticUI from "semantic-ui-css/semantic.min.css";
+import Spinner from './Spinner';
 
 
 
@@ -14,7 +15,7 @@ class App extends React.Component {
  // }
 
     state = { lat: null, errorMessage: '' };
-  componentDidMount() {
+    componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
@@ -22,7 +23,6 @@ class App extends React.Component {
   }
 
 
-   // React says we have to define render!!
 
 //   render() {
 //     return (
@@ -35,16 +35,22 @@ class App extends React.Component {
 //   }
 // }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
+        return <div>Error: {this.state.errorMessage}</div>;
+      }
 
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
-    }
+      if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />
+      }
 
-    return <div>Loading!</div>;
+      return <Spinner message="Please accept location request"/>;
+  }
+
+  render() {
+    return (
+      <div className="border red">{this.renderContent()}</div>
+    );
   }
 }
 
